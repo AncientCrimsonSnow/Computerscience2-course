8397f08c52e098cc1acdf1c4aa46c9593488608b
Juri Wiechmann <43830164+LuffyGM@users.noreply.github.com>
2020-02-10 16:16:15 +0100

    Add files via upload

---
+++ Task 8_Fun with Calculators 3/src/Task7_extensions/Usefull_Methods.java
@@ -0,0 +1,166 @@
+package Task7_extensions;
+
+
+public interface Usefull_Methods {
+
+	public static String deleteSpaces(String s) {
+		
+		return s.replace("\\s+", "");
+	}
+	
+	public static boolean StringisNumber(String number) {
+		
+		if(number.startsWith("."))
+			return false;
+					
+		char[] cNumber = number.toCharArray();
+		
+		int i = 0;
+		try {
+			if(cNumber[0] == '-' && CharisNumberHEX(cNumber[1]))
+				i = 1;
+		}
+		catch (Exception e) {}
+		
+		for (; i < cNumber.length ; i++) {
+			if(!(CharisNumberHEX(cNumber[i]) || cNumber[i] == '.'))
+				return false;
+		}
+		return true;
+	}
+	public static boolean CharisNumberHEX(char number) {
+		if( number > 47 && number < 58 ||
+			number > 64 && number < 71)
+			return true;
+		else
+			return false;
+	}
+	public static String setSpacesEachToken(String S) {
+		S = deleteSpaces(S);
+		char [] S_Array = S.toCharArray();
+		
+		//If the input String has 2 dots in a row its wrong Tested here.
+		int count = 0;
+		for(char E : S_Array) {
+			if(E == '.')
+				count++;
+			else
+				count = 0;
+			if(count >1)
+				return null;	
+		}
+	
+		String token = "";
+		S = "";
+		for(int i = 0; i< S_Array.length;i++) {
+			token = "";
+			
+			//negativ numbers
+			if(	S_Array[i] == '-') {
+				token += S_Array[i];
+				
+				try {
+					//if its an neg followed by a number
+					if(CharisNumberHEX(S_Array[i+1])){
+						//If - is the first digit:
+						if(i==0) {	
+							while(CharisNumberHEX(S_Array[i+1]) || S_Array[i+1] == '.') {
+								i++;
+								token += S_Array[i];	
+							}
+						}
+						else {
+							//if the - is not at the first digit
+							if(CharisOperator(S_Array[i-1])) {		
+								while(CharisNumberHEX(S_Array[i+1]) || S_Array[i+1] == '.') {
+									i++;
+									token += S_Array[i];						
+								}
+							}	
+						}
+					}
+					//if the neg is followed by an (
+					else if(S_Array[i+1] == '(') {
+						i++;
+						token += S_Array[i];
+					}
+				}
+				catch(Exception e) {}
+				S += token + " ";
+				continue;
+			}
+			//operatoren
+			if( i > 0  && CharisOperator(S_Array[i])) {
+				token += S_Array[i];
+				S += token + " ";
+				continue;
+			}
+			//positive Zahlen
+			else if(CharisNumberHEX(S_Array[i])){
+				token = Character.toString(S_Array[i]);
+				try {
+					while(CharisNumberHEX(S_Array[i+1]) || S_Array[i+1] == '.') {
+						i++;
+						token += S_Array[i];	
+					}
+				}
+				catch(Exception e) {}
+				S += token + " ";
+				continue;
+			}
+			else {
+				//If input is wrong.
+				return null;
+			}
+		}
+		
+		
+		return S.stripTrailing();
+	}
+	public static boolean CharisToken(char c) {
+		if(CharisNumberHEX(c) || CharisOperator(c)) {
+			return true;
+		}
+		else 
+			return false;
+	}
+	public static boolean CharisOperator(char c) {
+		if( c == '+' ||
+			c == '-' ||
+			c == 'x' ||
+			c == '*' ||
+			c == '/' ||
+			c == '^' ||
+			c == '(' ||
+			c == ')' ||
+			c == '=') 
+			return true;
+		else
+			return false;
+	}
+
+	public static String getSep(String input) {
+		//if you want too really seaching for an sepperator u need a more complicated way, this is the bugded version
+		if(input.contains("/"))
+			return  "/";
+		else if(input.contains("-"))
+			return  "-";
+		else if(input.contains("."))
+			return  ".";
+		else if(input.contains("_"))
+			return  "_";
+		return null;
+	}
+	public static int twoPointsDistance(int x1, int y1, int x2, int y2) {
+
+		return (int) Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+	}
+	public static int[] getMidpoint(int x1, int y1, int x2, int y2) {
+		int[] p1 = {
+				(x1+x2)/2,
+				(y1+y2)/2,
+		};
+		return p1;
+	}
+}
+
